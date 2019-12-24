96bdc50e1cd4d9c9c8ba16e01a983ccde5221171
Amar Nath Yogi <immortal2907@gmail.com>
2019-12-24 20:16:26 +0530

    Updates

---
+++ precache-manifest.49b84e93f8eacb44bb9cf1946cf0897a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25dd9fa820d01c80a7d7bffe14166b7",
+    "url": "/YouClone/index.html"
+  },
+  {
+    "revision": "c8fe348015c3fe61504a",
+    "url": "/YouClone/static/css/main.8b101456.chunk.css"
+  },
+  {
+    "revision": "b2322fd1cb9c9f5456f1",
+    "url": "/YouClone/static/js/2.16610dcc.chunk.js"
+  },
+  {
+    "revision": "c8fe348015c3fe61504a",
+    "url": "/YouClone/static/js/main.f02b12fb.chunk.js"
+  },
+  {
+    "revision": "6bf0b24bfa0826df4e16",
+    "url": "/YouClone/static/js/runtime~main.77602613.js"
+  }
+]);
